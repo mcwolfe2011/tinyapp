7da4f4559f76a59386e4a288b1d4aaa100586584
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-22 23:26:01 -0700

    Did the registration form and its route

--- express_server.js
+++ express_server.js
@@ -115,11 +115,39 @@ app.post('/logout', (req, res) => {
 
 
 
+// Day 3 Added Codes - Have mentor check it
+// Authentication
+// Display the register form
+app.get('/register', (req, res) => {
+  const templateVars = { currentUser: null };
+  res.render('register', templateVars);
+});
 
+// Get the info from the register form
+app.post('/register', (req, res) => {
+  // extract the info from the form
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
 
+  // check if the user is not already in the database
 
+  const user = findUserByEmail(email);
 
+  // if not in the db, it'ok to add the user to the db
 
+  if (!user) {
+    const userId = addNewUser(name, email, password);
+    // setCookie with the user id
+    res.cookie('user_id', userId);
+
+    // redirect to /quotes
+    res.redirect('/urls');
+  } else {
+    res.status(404).send('Sorry, the user is already registered');
+  }
+});
+//end of day 3 added codes
 
 
 
